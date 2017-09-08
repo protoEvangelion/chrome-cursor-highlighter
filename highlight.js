a9811269f09170ef6191fbb171209bb5aae6bc0c
protoEvangelion <ryantgarant@gmail.com>
2017-09-08 11:46:14 -0700

    Added move highlighter on scroll

--- highlight.js
+++ highlight.js
@@ -1,19 +1,43 @@
+let xMousePos = 0
+let yMousePos = 0
+let lastScrolledLeft = 0
+let lastScrolledTop = 0
+
 $('body').append('<div id="super-highlighter"></div>')
 $('body').append('<div id="super-dot"></div>')
 
-$('body').on('mousemove', positionHighlighter)
+$(document).on('mousemove', positionOnMouseMove)
+$(window).on('scroll', positionOnScroll)
 
-function positionHighlighter(e) {
-	const mouseX = e.pageX
-	const mouseY = e.pageY
+function positionOnMouseMove(e) {
+  xMousePos = e.pageX
+  yMousePos = e.pageY
 
-	$('#super-highlighter').offset({
-		top: mouseY - 25,
-		left: mouseX - 25
-	})
+  moveLeft()
+  moveUp()
+}
 
-	$('#super-dot').offset({
-		top: mouseY - 2.5,
-		left: mouseX - 2.5 
-	})
+function positionOnScroll() {
+  if (lastScrolledLeft != $(document).scrollLeft()) {
+    xMousePos -= lastScrolledLeft
+    lastScrolledLeft = $(document).scrollLeft();
+    xMousePos += lastScrolledLeft
+    moveLeft()
+  }
+  if (lastScrolledTop != $(document).scrollTop()) {
+    yMousePos -= lastScrolledTop
+    lastScrolledTop = $(document).scrollTop()
+    yMousePos += lastScrolledTop
+    moveUp()
+  }
+}
+
+function moveLeft() {
+  $('#super-dot').offset({ left: xMousePos - 2.5 })
+  $('#super-highlighter').offset({ left: xMousePos - 25 })
+}
+
+function moveUp() {
+  $('#super-dot').offset({ top: yMousePos - 2.5 })
+  $('#super-highlighter').offset({ top: yMousePos - 25 })
 }
